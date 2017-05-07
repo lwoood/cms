91ec193b81a49243e59a0ab9ca07735324332687
lwoood <lwoood@gmx.de>
2017-05-07 22:33:17 +0200

    help text

--- run.sh
+++ run.sh
@@ -4,10 +4,11 @@ php -S localhost:8888
 echo "";
 echo "*********************************************************";
 echo "";
-echo " trying starting local php webserver on 'http://localhost:8888' with";
+echo "trying starting local php webserver on 'http://localhost:8888' with";
 echo "";
 echo "  # cd web/";
 echo "  # php -S localhost:8888";
 echo "";
+echo "You should change the panel admin password right now!";
 echo "";
 echo "*********************************************************";
