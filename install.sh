b2431ab093eb181c47efe767a4dd3136b6f9ac73
lwoood <lwoood@gmx.de>
2017-05-09 21:04:27 +0200

    testing

--- install.sh
+++ install.sh
@@ -28,11 +28,12 @@ cp  infrastructure/robots.txt web/robots.txt
 echo "Installing some kirby plugins";
 cd web;
 kirby plugin:install storypioneers/kirby-selector
-kirby plugin:install JonasDoebertin/kirby-visual-markdown
 kirby plugin:install 1n3JgKl9pQ6cUMrW/kirby-tabs
-#kirby plugin:install distantnative/panel-bar
+kirby plugin:install distantnative/panel-bar
 kirby plugin:install distantnative/field-switch
 kirby plugin:install SebastianEberlein/kirby-markdown-field
+# for mor WYSIWYG feeling
+#kirby plugin:install JonasDoebertin/kirby-visual-markdown
 
 echo "Creating user 'admin' with password 'admin'";
 kirby make:user -u admin -p admin
